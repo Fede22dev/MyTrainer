960b36a0f5e09f66096a8af4e2830dfa9e11e344
Fede22dev <79111932+Fede22dev@users.noreply.github.com>
2021-05-09 12:02:14 +0200

    Creata activity scelta utente e impostata la grafica

---
+++ app/src/main/java/it/app/mytrainer/RegistrationAthlete.kt
@@ -0,0 +1,11 @@
+package it.app.mytrainer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationAthlete: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/it/app/mytrainer/UserChoice.kt
@@ -0,0 +1,11 @@
+package it.app.mytrainer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserChoice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
