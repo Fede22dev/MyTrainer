0335de01bb7e066b9227688bf0f588ebe274edb3
Lampe96 <lampe96@hotmail.it>
2021-05-29 14:47:48 +0200

    Creata la home lato athlete con bottom nave. Effettuate alcune modifiche grafiche ai layout riguardanti la registrazione. Correzioni varie

--- app/src/main/java/it/app/mytrainer/ui/activities/home/ActivityHomeAthlete.kt
+++ app/src/main/java/it/app/mytrainer/ui/activities/home/ActivityHomeAthlete.kt
@@ -1,12 +1,39 @@
 package it.app.mytrainer.ui.activities.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import it.app.mytrainer.R
+import it.app.mytrainer.firebase.fireauth.FireAuth
+import it.app.mytrainer.ui.activities.starter.ActivityLogin
+import kotlinx.android.synthetic.main.activity_home_athlete.*
+import kotlinx.android.synthetic.main.activity_home_trainer.*
 
 class ActivityHomeAthlete : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_athlete)
+
+        val navController = findNavController(R.id.navHostFragmentHomeAthlete)
+        bottomNavHomeAthlete.setupWithNavController(navController)
+
+        topAppBarHomeAthlete.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+
+                R.id.signOut -> {
+                    FireAuth.signOut()
+                    val intent = Intent(this, ActivityLogin::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+
+                else -> false
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/it/app/mytrainer/ui/fragments/homeAthlete/FragmentScheduleAthlete.kt
@@ -0,0 +1,19 @@
+package it.app.mytrainer.ui.fragments.homeAthlete
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import it.app.mytrainer.R
+
+class FragmentScheduleAthlete : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule_athlete, container, false)
+    }
+}
